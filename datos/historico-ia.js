3447573eff0239d89d690af914c167c3cb6769c6
aleixxruiz <aleixcr9@gmail.com>
2026-06-04 09:00:23 +0200

    Actualizacion dashboard (2026-06-04 09:00)

--- datos/historico-ia.js
+++ datos/historico-ia.js
@@ -1,1 +1,1 @@
-﻿window.CLARITY_IA_HISTORICO = [{"fecha":"2026-06-03","sesionesIA":16,"usuariosIA":17,"sesionesTotal":2140}];
+﻿window.CLARITY_IA_HISTORICO = [{"fecha":"2026-06-03","sesionesIA":16,"usuariosIA":17,"sesionesTotal":2140},{"fecha":"2026-06-04","sesionesIA":25,"usuariosIA":26,"sesionesTotal":2920}];
