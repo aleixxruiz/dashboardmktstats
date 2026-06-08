90db9685f8cdd5edfe95a7d452e3b17882e2d3ac
aleixxruiz <aleixcr9@gmail.com>
2026-06-08 17:50:19 +0200

    Actualizacion dashboard (2026-06-08 17:50)

--- datos/leads.js
+++ datos/leads.js
@@ -1,1 +1,1 @@
-﻿window.LEADS = {"porIdioma":[],"total":0,"porFormulario":[],"nuevos30":0,"porEtapa":{},"conFormulario":0,"porMes":{},"nuevosPrev30":0,"porFuente":{},"fechaActualizacion":"08/06/2026 09:00"};
+﻿window.LEADS = {"porIdioma":[],"total":0,"porFormulario":[],"nuevos30":0,"porEtapa":{},"conFormulario":0,"porMes":{},"nuevosPrev30":0,"porFuente":{},"fechaActualizacion":"08/06/2026 17:44"};
