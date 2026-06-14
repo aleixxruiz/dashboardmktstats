8fdbf400a8a57b3455d5e8d54a8889c486176ac4
aleixxruiz <aleixcr9@gmail.com>
2026-06-14 11:21:29 +0200

    Actualizacion dashboard (2026-06-14 09:21)

--- datos/historico-ia.js
+++ datos/historico-ia.js
@@ -1,1 +1,1 @@
-window.CLARITY_IA_HISTORICO = [{"fecha":"2026-06-13","sesionesIA":4,"usuariosIA":4,"sesionesTotal":512,"plat":{"ChatGPT":4}}];
+window.CLARITY_IA_HISTORICO = [{"fecha":"2026-06-14","sesionesIA":2,"usuariosIA":2,"sesionesTotal":217,"plat":{"ChatGPT":2}}];
