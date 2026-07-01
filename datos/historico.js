42f3be113d2e3c87dc054ae20b860d71529c2570
aleixxruiz <aleixcr9@gmail.com>
2026-07-01 11:33:29 +0200

    Actualizacion dashboard (2026-07-01 09:33)

--- datos/historico.js
+++ datos/historico.js
@@ -1,1 +1,1 @@
-window.CLARITY_HISTORICO = [{"fecha":"2026-06-30","sesiones":1369,"usuarios":3056,"bots":1633}];
+window.CLARITY_HISTORICO = [{"fecha":"2026-07-01","sesiones":2225,"usuarios":3940,"bots":1650}];
